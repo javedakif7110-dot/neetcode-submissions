a1497dc6fd121a00a6c56079dd12fc4bdef5c541
AKIF JAVED <javedakif7110@gmail.com>
2026-08-10 13:51:59 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+
+ stack<int>s,minS;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if(minS.empty()){
+            minS.push(val);
+        }
+        else{
+             minS.push(min(val,minS.top()));
+        }
+    }
+
+    
+    void pop() {
+        s.pop();
+        minS.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minS.top();
+    }
+};
